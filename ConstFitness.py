9770dd9c650d34bc297a666f05d59311b08df416
Jeremy Dening <jpdening@buffalo.edu>
2025-03-04 15:19:37 -0500

    Changed python module to permit use of Optimized constellations. Tutorial updated to reflect these changes.

--- ConstFitness.py
+++ ConstFitness.py
@@ -9,97 +9,82 @@ class Constellation():
     T_f = 192.0
     dt = 30.0
     alpha = 30
-
-    # Variables to be filled with read_types
-    root_path = 0
-    site_dict = {}
-    orbit_types = []
+    valid_site_locs = [x for x in range(1, 10)]
 
     # Variables to be filled with get_site_locs
     site_locs = []
     
     # The constructor must take in a list of tuples
-    def __init__(self, in_sats):
+    def __init__(self, in_sats, type="Random"):
+
+        # Check that the type is correct
+        if type not in ["Random", "Optimized"]:
+            raise Exception("Please use a valid type. Use either Random or Optimized")
 
         # Check that in_sats is of the proper shape. It should be a list of lists
         proper_len = True
-        for ii in range(len(in_sats)):
-            if len(in_sats[ii]) != 2:
-                proper_len = False
-                break
-        
-        if not proper_len:
-            raise Exception("Please ensure that all satellites are composed of orbit_type and phasing string pairs")
 
-        if len(Constellation.site_dict) == 0:
-            raise Exception("Please call Constellation.read_types first")
+        if type == "Random":
+            for ii in range(len(in_sats)):
+                if len(in_sats[ii]) != 2:
+                    proper_len = False
+                    break
+        else:
+            if len(in_sats) != 3:
+                proper_len = False
+
+        if not proper_len:
+            if type == "Random":
+                raise Exception("Please ensure that all satellites are composed of orbit_type and phasing string pairs")
+            if type == "Optimized":
+                raise Exception("Please ensure that the optimized constellation is represented by a 3-tuple string, RED/BLUE type, and constellation number strings")
 
         # Add all of the satellites
-        self.add_satellites(in_sats)
+        self.add_satellites(in_sats, type)
 
     # This method overwrites all of the satellites present in the constellation, and then computes the data associated with them
-    def add_satellites(self, in_sats):
-        self.sats = in_sats
+    def add_satellites(self, in_sats, type):
         self.sat_data = []
-        for s in range(len(self.sats)):
-            self.sat_data.append(Constellation.get_visibility_data(self.sats[s]))
 
-    # The class method must return the visibility data associated with a given satellite
-    @classmethod
-    def get_visibility_data(cls, in_sat):
-
-        # in_sat is expected to be a list of the form ["orbit_type, "phasing"]
-
-        # We need the visibility arrays for all of the site locations for the given satellite. 
-        # These visibility arrays will be stored in tuples
-
-        # Throw an error if there are no site locations listed
-        if len(cls.site_locs) == 0:
-            raise Exception("Please use get_site_locs to enter at least one site location before calling this")
-        
-        # Throw an error if the path is not declared yet
-        if cls.root_path == 0:
-            raise Exception("Please use read_types to enter the root path")
-
-        # Check that the orbit type is valid
-        if in_sat[0] not in cls.orbit_types:
-            raise Exception("Please use one of the correct orbit types. Refer to the CLPSSitesRandomSats directory for valid types")
-
-        # Read in the phasing. Correct to 0.1 if it is 0 to agree with pandas renaming convention
-        phasing = in_sat[1]
-        if phasing == "0":
-            phasing = "0.1"
-
-        vis_store = []
-        for s in cls.site_locs:
-            my_path = cls.site_dict[s] / f"{in_sat[0]}/site_detection.csv"
-
-            # Read the data
-            my_df = pd.read_csv(my_path)
-
-            # Check that the phasing is valid
-            all_columns = my_df.columns.values
-            all_columns = all_columns[1:].tolist()
-
-            if phasing not in all_columns:
-                raise Exception("Please use one of the correct phasing types. Refer to a data file in the CLPSSitesRandomSats directory for valid types")
-
-            vis_dat = my_df[phasing].values
-            vis_store.append(vis_dat)
-        
-        return vis_store
+        if type == "Random":
+            self.sat_data = RandConst.all_vis_data(in_sats)
+        else:
+            self.sat_data = OptConst.all_vis_data(in_sats)
 
     # The class method must take in the site locations used
     @classmethod
     def get_site_locs(cls, in_site_locs):
 
-        if len(cls.site_dict) == 0:
-            raise Exception("Please call Constellation.read_types first")
-
-        if max(in_site_locs) > len(cls.site_dict) or min(in_site_locs) < 1:
+        if max(in_site_locs) > cls.valid_site_locs[-1] or min(in_site_locs) < 1:
             raise Exception("Invalid index for a site location. Please choose again")
 
         cls.site_locs = in_site_locs
+
+    @staticmethod
+    def get_root_dir(rand_dir=0, opt_dir=0):
+
+        if rand_dir != 0:
+            if not rand_dir.exists():
+                raise Exception("Invalid path to random directory")
+            else:
+                RandConst.read_types(rand_dir)
+        
+        if opt_dir != 0:
+            if not opt_dir.exists():
+                raise Exception("Invalid path to optimized directory")
+        
+        RandConst.rand_path = rand_dir
+        OptConst.opt_path = opt_dir
+
+
+# Do NOT call this class from outside
+class RandConst(Constellation):
+
+    ## Class attributes
+    # Default variables
+    site_dict = {}
+    orbit_types = []
+    rand_path = 0
 
     # The class method must take in the location of CLPSSitesRandomSats and instantiate the applicable orbits and site locations
     @classmethod
@@ -123,19 +108,134 @@ class Constellation():
         orbit_format = f"*_[0-9]*"
         file_gen = Path(site_dirs[0]).glob(orbit_format)
         cls.orbit_types = [file.name for file in file_gen]
+    
+    # The class method must return the visibility data associated with a given satellite
+    @classmethod
+    def get_visibility_data(cls, in_sat):
+
+        # in_sat is expected to be a list of the form ["orbit_type, "phasing"]
+
+        # We need the visibility arrays for all of the site locations for the given satellite. 
+        # These visibility arrays will be stored in tuples
+
+        # Throw an error if there are no site locations defined yet
+        if len(Constellation.site_locs) == 0:
+            raise Exception("Please declare some site locations using Constellation.get_site_locs")
+        
+        # Throw an error if the path is not declared yet
+        if cls.rand_path == 0:
+            raise Exception("Please use Constellation.get_root_dir to enter the root path")
+
+        # Check that the orbit type is valid
+        if in_sat[0] not in cls.orbit_types:
+            raise Exception("Please use one of the correct orbit types. Refer to the CLPSSitesRandomSats directory for valid types")
+
+        # Read in the phasing. Correct to 0.1 if it is 0 to agree with pandas renaming convention
+        phasing = in_sat[1]
+        if phasing == "0":
+            phasing = "0.1"
+
+        vis_store = []
+        for s in Constellation.site_locs:
+            my_path = cls.site_dict[s] / f"{in_sat[0]}/site_detection.csv"
+
+            # Read the data
+            my_df = pd.read_csv(my_path)
+
+            # Check that the phasing is valid
+            all_columns = my_df.columns.values
+            all_columns = all_columns[1:].tolist()
+
+            if phasing not in all_columns:
+                raise Exception("Please use one of the correct phasing types. Refer to a data file in the CLPSSitesRandomSats directory for valid types")
+
+            vis_dat = my_df[phasing].values
+            vis_store.append(vis_dat)
+        
+        return vis_store
+    
+    @classmethod
+    def all_vis_data(cls, in_sats):
+        all_data = []
+        for s in range(len(in_sats)):
+            all_data.append(RandConst.get_visibility_data(in_sats[s]))
+        return all_data
+
+
+class OptConst(Constellation):
+
+    ## Class attributes
+    # Default variables
+    opt_path = 0
+
+    @classmethod
+    def all_vis_data(cls, in_sats):
+
+        # Form the path to the data
+        site_loc_string = "("
+        for s in cls.site_locs[:-1]:
+            site_loc_string = site_loc_string + f"({s},{cls.alpha}),"
+        site_loc_string = site_loc_string + f"({cls.site_locs[-1]},{cls.alpha}))"
+
+        base_path = cls.opt_path / in_sats[0] / f"T_fin_{cls.T_f}_dt_{cls.dt}_site_locs_{site_loc_string}"
+
+        if not base_path.exists():
+            raise Exception("Cannot find optimized data. Check the 3-tuple OR the site locations for correctness")
+        
+        my_path = base_path / in_sats[1]
+
+        # See how many constellations are in this directory
+        my_format = "const_*"
+        all_const_iter = my_path.glob(my_format)
+
+        num_const = 0
+
+        for _ in all_const_iter:
+            num_const += 1
+
+        if in_sats[2] > num_const:
+            raise Exception(f"Invalid constellation number. Exceeds the number of constellations present. Choose a constellation between 1 and {num_const}")
+        
+        # Get the path to my constellation
+        my_const = my_path / f"const_{in_sats[2]}"
+
+        # Determine the number of satellites
+        all_sats_iter = my_const.glob("sat_*")
+        num_sats = 0
+
+        for _ in all_sats_iter:
+            num_sats += 1
+
+        # Now, extract the data. Note that visibility data is defined in layers. The outer layer is for satellites, layer 2 is for sites, layer 3 is the actual data
+        all_data = []
+
+        for sat in range(1, num_sats+1):
+            data_path = my_const / f"sat_{sat}/site_detection.csv"
+
+            my_df = pd.read_csv(data_path)
+
+            sat_data = []
+            for site in range(1, len(cls.site_locs)+1):
+                col_num = f"0.{site}"
+                temp_data = my_df[col_num].values
+                temp_data = np.concatenate((np.array([0]), temp_data))
+
+                sat_data.append(temp_data)
+            
+            all_data.append(sat_data)
+        return all_data
 
 
 # Make a derived class "BlueConst"
-
 class BlueConst(Constellation):
 
-    def __init__(self, in_sats):
-        super().__init__(in_sats)
-
+    def __init__(self, in_sats, type="Random"):
+        super().__init__(in_sats, type)
+        
     # This will output a 
     def get_blue_fitness(self):
 
-        n_sat = len(self.sats) # The number of satellites
+        n_sat = len(self.sat_data) # The number of satellites
         n_sites = len(Constellation.site_locs)
         # For each site location, we will compute the coverage and the maximum visible satellites
         blue_fit = np.zeros((1, 2 * n_sites))
@@ -163,13 +263,13 @@ class BlueConst(Constellation):
 
 class RedConst(Constellation):
 
-    def __init__(self, in_sats, in_blue_const:BlueConst):
-        super().__init__(in_sats)
+    def __init__(self, in_sats, in_blue_const:BlueConst, type="Random"):
+        super().__init__(in_sats, type)
         self.blue_const = in_blue_const
 
     def get_red_fitness(self):
-        n_blue = len(self.blue_const.sats)
-        n_red = len(self.sats) # The number of satellites
+        n_blue = len(self.blue_const.sat_data)
+        n_red = len(self.sat_data) # The number of satellites
         n_sites = len(Constellation.site_locs)
 
         red_fit = np.zeros((1, 2 * n_sites))
--- tutorial.py
+++ tutorial.py
@@ -3,32 +3,31 @@ from pathlib import Path
 import numpy as np
 
 # Declare the path to the CLPSSitesRandomSats directory
-root_path = Path('./CLPSSitesRandomSats')
+rand_path = Path('./CLPSSitesRandomSats')
+opt_path = Path('./AllConstellations')
 
 # Call read_types from the Constellation class. This pulls in all of the possible site locations and orbit types from the CLPSSitesRandomSats directory
-Constellation.read_types(root_path)
+Constellation.get_root_dir(rand_dir=rand_path, opt_dir=opt_path)
 
 # Declare what site locations will be used. The convention is the same as in the file directory, with a numbering [1-9]
-site_locs = [2, 3]
+site_locs = [1, 3]
 
 # Call get_site_locs to set what the site locations are in the Constellation base class
 Constellation.get_site_locs(site_locs)
 
-# Now, declare some satellites! These must have the following form. This is an nx2 array, with n satellites. Each satellite is specified by the name of the orbit and the phasing
-blue_sats = [["DRO_1", "20"],
-           ["halo_1", "0"],
-           ["halo_3", "80"]]
+# Now, declare some satellites! These must have the following form for the optimized satellites
+blue_opt_sats = ["(3,3,2)", "BLUE", 1]
 
-# Declare the blue constellation by feeding in the blue satellites
-blue_const = BlueConst(blue_sats)
+# Declare the blue constellation by feeding in the blue satellites. For optimized satellites, the type must be declared as "Optimized". 
+blue_const = BlueConst(blue_opt_sats, type="Optimized")
 
-# Declare the red satellites
-red_sats = [["DRO_1", "20"],
-            ["halo_1", "0"],
-            ["halo_3", "80"]]
+# Declare the red satellites. Randomized satellites must have the following form.
+red_rand_sats = [["DRO_1", "20"],
+                 ["halo_1", "0"],
+                 ["halo_3", "80"]]
 
 # Declare the red constellation. This constellation is tracking the blue constellation, which is fed in as the second parameter in the constructor
-red_const = RedConst(red_sats, blue_const)
+red_const = RedConst(red_rand_sats, blue_const)
 
 # These print out the generated fitness values. Each sequential set of two columns matches up to the site locations as they were declared in site_locs. 
 # So, the first two columns correspond to site location 2, and the next two columns to site location 3
